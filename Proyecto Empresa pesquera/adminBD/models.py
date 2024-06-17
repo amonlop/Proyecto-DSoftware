748622085ad1f9a98e6842b081564be2411976e4
Antonia <montlopanto@gmail.com>
2024-06-17 00:12:25 -0400

    cambios a models.py, se añadieron campos a las clases

--- Proyecto Empresa pesquera/adminBD/models.py
+++ Proyecto Empresa pesquera/adminBD/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 from django.db.models import UniqueConstraint
+from datetime import datetime
 
 # Create your models here.
-class cardumen(models.Model):
+class Cardumen(models.Model):
     nombre = models.CharField(primary_key=True, max_length=200)
+    profundidad_min = models.IntegerField()
+    profundidad_max = models.IntegerField()
+    temp_min = models.IntegerField()
+    temp_max = models.IntegerField()
+    x_min = models.IntegerField()
+    x_max = models.IntegerField()
+    y_min = models.IntegerField()
+    y_max = models.IntegerField()
+
     def __str__(self):
         return self.nombre
 
-class zona(models.Model):
+class Zona(models.Model):
     coordenadaX = models.IntegerField()
     coordenadaY = models.IntegerField()
+    profundidad = models.IntegerField()
+    temperatura = models.IntegerField()
 
     def __str__(self) -> str:
             return 'Zona '+ str(self.pk)
@@ -18,3 +30,26 @@ class zona(models.Model):
         constraints = [
             UniqueConstraint(fields=['coordenadaX', 'coordenadaY'], name='zona_key')
         ]
+
+
+class Viaje(models.Model):
+    fecha_salida = models.DateField()
+    fecha_llegada = models.DateField()
+    es_viaje_exitoso = models.IntegerField()
+
+    def __str__(self) -> str:
+         return "Viaje" + str(self.pk)
+
+
+    def duracion_viaje():
+        return (self.fecha_llegada - self.fecha_salida).days
+
+
+class Embarcacion(models.Model):
+    nombre = models.CharField()
+    capacidad_tripulacion = models.IntegerField()
+    capacidad_bodega = models.IntegerField() #cuantos kls de mercadería pueden almacenar
+
+
+    def __str__(self) -> str:
+         return "Embarcación" + str(self.pk)
--- Proyecto Empresa pesquera/genioPesquero/settings.py
+++ Proyecto Empresa pesquera/genioPesquero/settings.py
@@ -76,21 +76,10 @@ WSGI_APPLICATION = 'genioPesquero.wsgi.application'
 
 
 DATABASES = {
-<<<<<<< Updated upstream
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'EmpresaPesquera',
-    #     'USER': 'postgres',
-    #     'PASSWORD':'XiQ34Lkw6B',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432'
-    # }
-=======
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR /'db.sqlite3',
     }
->>>>>>> Stashed changes
 }
 
 
